8886a864eec16d42a3ac9c8b3f2ae8de9c6fece7
Sara Gironi <sara.gironi97@gmail.com>
2025-01-24 17:59:37 +0100

    second pull request changes

--- generic_classes/generic_food.py
+++ generic_classes/generic_food.py
@@ -1,29 +1,53 @@
+from dataclasses import dataclass
 from datetime import datetime
 
 
+@dataclass
 class GenericFood:
-    def __init__(
-        self,
-        name: str,
-        purchase_date: datetime,
-        expiration_date: datetime,
-        quantity: float,
-    ):
-        self.name = name
-        self.purchase_date = purchase_date
-        if expiration_date >= purchase_date:
-            self.expiration_date = expiration_date
-        else:
-            raise Exception("Expiration date must be after purchase date!")
-        if quantity > 0:
-            self.quantity = quantity
-        else:
-            raise Exception("Quantity must be greater than 0!")
+    name: str
+    purchase_date: datetime
+    expiration_date: datetime
+    quantity: float
 
-    def modify_quantity(cls, new_quantity):
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def purchase_date(self):
+        return self._purchase_date
+
+    @purchase_date.setter
+    def purchase_date(self, value):
+        self._purchase_date = value
+
+    @property
+    def expiration_date(self):
+        return self._expiration_date
+
+    @expiration_date.setter
+    def expiration_date(self, value):
+        assert (
+            value >= self.purchase_date
+        ), "Expiration date must be after purchase date!"
+        self._expiration_date = value
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        assert value > 0, "Quantity must be greater than 0!"
+        self._quantity = value
+
+    def modify_quantity(self, new_quantity):
         if new_quantity is not None and new_quantity > 0:
-            cls.quantity = new_quantity
+            self.quantity = new_quantity
 
     def __str__(self):
-        food = f"Type of food: {self.name}, purchased on: {self.purchase_date}, expiration date: {self.expiration_date}, quantity: {self.quantity}"
-        return food
+        return f"Type of food: {self.name}, purchased on: {self.purchase_date}, expiration date: {self.expiration_date}, quantity: {self.quantity}"
--- generic_classes/generic_food_storage.py
+++ generic_classes/generic_food_storage.py
@@ -1,9 +1,38 @@
+from dataclasses import dataclass
+from generic_classes.generic_food import GenericFood
+
+
+@dataclass
 class GenericFoodStorage:
-    def __init__(self, name: str, capacity: float, foods: list):
-        self.name = name
-        if capacity > 0:
-            self.capacity = capacity
-        self.foods = foods
+    def __init__(self, name: str, capacity: float, foods: list[GenericFood]):
+        self._name = name
+        self._capacity = capacity
+        self._foods = list(foods)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @capacity.setter
+    def capacity(self, value):
+        assert value >= 0, "Capacity must be greater than 0!"
+        self._capacity = value
+
+    @property
+    def foods(self):
+        return self._foods
+
+    @foods.setter
+    def foods(self, value):
+        self._foods = value
 
     def reduce_capacity(self, consumed_quantity):
         self.capacity -= consumed_quantity
@@ -11,35 +40,36 @@ class GenericFoodStorage:
     def increase_capacity(self, added_quantity):
         self.capacity += added_quantity
 
-    def add_food(self, new_food):
-        for f in new_food:
-            if self.capacity >= f.quantity:
-                self.foods.append(f)
-                self.reduce_capacity(f.quantity)
-            elif self.capacity < f.quantity:
-                raise Exception("You don't have enough space to store this food!")
+    def add_food(self, new_food: list[foods]):
+        total_quantity = sum(f.quantity for f in new_food)
+        if self.capacity >= total_quantity:
+            self.foods.extend(new_food)
+            self.reduce_capacity(total_quantity)
+        else:
+            raise Exception("You don't have enough space to store this food!")
 
-    def consume_food(self, consumed_food, consumed_quantity):
+    def consume_food(self, consumed_food: dict["food":str, "quantity":float]):
         for f in self.foods:
-            if f.name == consumed_food.name and consumed_quantity == f.quantity:
-                self.foods.remove(consumed_food)
+            consumed_foodtype = consumed_food.get("food")
+            consumed_quantity = consumed_food.get("quantity")
+
+            if f.name == consumed_foodtype and f.quantity == consumed_quantity:
+                self.foods.remove(f)
                 self.increase_capacity(consumed_quantity)
-            elif f.name == consumed_food.name and consumed_quantity < f.quantity:
+            elif f.name == consumed_foodtype and consumed_quantity < f.quantity:
                 f.quantity -= consumed_quantity
                 self.increase_capacity(consumed_quantity)
-            elif f.name == consumed_food.name and consumed_quantity > f.quantity:
+            elif f.name == consumed_foodtype and consumed_quantity > f.quantity:
                 raise Exception("You cannot consume more than you have!")
+            else:
+                raise Exception(f"No {consumed_foodtype} in the storage!")
 
-    def food_availability(self, new_date):
+    def food_availability(self, date):
         return [
             (food.name, food.quantity)
             for food in self.foods
-            if food.purchase_date <= new_date and food.expiration_date >= new_date
+            if food.purchase_date <= date and food.expiration_date >= date
         ]
-
-    def get_quantity_and_name(self):
-        for f in self.foods:
-            print(f.name, f.quantity)
 
     def __str__(self):
         return f"{self.name} with capacity: {self.capacity}, containing: {[str(f) for f in self.foods]}"
