e7fec5f898a04b175d63878a93c1cdaaa4bb3636
Sara Gironi <sara.gironi97@gmail.com>
2025-02-04 11:50:06 +0100

    modified gitignore and GenericFood

--- generic_classes/generic_food.py
+++ generic_classes/generic_food.py
@@ -4,9 +4,8 @@ from datetime import datetime
 
 @dataclass
 class GenericFood:
-    name: str
-    purchase_date: datetime
-    expiration_date: datetime
+    _name: str
+    _expiration_date: datetime
 
     @property
     def name(self):
@@ -17,23 +16,12 @@ class GenericFood:
         self._name = name
 
     @property
-    def purchase_date(self):
-        return self._purchase_date
-
-    @purchase_date.setter
-    def purchase_date(self, purchase_date: datetime):
-        self._purchase_date = purchase_date
-
-    @property
     def expiration_date(self):
         return self._expiration_date
 
     @expiration_date.setter
     def expiration_date(self, expiration_date: datetime):
-        assert (
-            expiration_date >= self.purchase_date
-        ), "Expiration date must be after purchase date!"
         self._expiration_date = expiration_date
 
     def __str__(self):
-        return f"Type of food: {self.name}, purchased on: {self.purchase_date}, expiration date: {self.expiration_date}"
+        return f"Type of food: {self._name}, expiration date: {self._expiration_date}"
