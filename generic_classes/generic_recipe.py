8770ef71e9776925e9e1c3c2e8bef4f3c882d6a0
Sara Gironi <sara.gironi97@gmail.com>
2025-03-12 16:05:00 +0100

    generic recipe class created

--- generic_classes/generic_recipe.py
+++ generic_classes/generic_recipe.py
@@ -18,38 +18,38 @@ class GenericRecipe:
     @name.setter
     def name(self, name: str) -> None:
         self._name = name
-    
+
     @property
     def ingredients(self) -> GenericFood:
         return self._ingredients
-    
+
     @ingredients.setter
     def ingredients(self, ingredients: GenericFood) -> None:
         self._ingredients = ingredients
-    
+
     @property
     def expiration_date(self) -> datetime:
         return self._expiration_date
-    
-    @expiration_date.setter 
+
+    @expiration_date.setter
     def expiration_date(self, expiration_date: datetime) -> None:
         self._expiration_date = expiration_date
-    
+
     @property
     def cooking_time(self) -> int:
         return self._cooking_time
-    
+
     @cooking_time.setter
     def cooking_time(self, cooking_time: int) -> None:
         self._cooking_time = cooking_time
-    
+
     @property
     def total_kcal(self) -> int:
         return self._total_kcal
-    
+
     @total_kcal.setter
     def total_kcal(self, total_kcal: int) -> None:
         self._total_kcal = total_kcal
-    
+
     def __str__(self) -> str:
         return f"Recipe name: {self._name}, ingredients: {self._ingredients}, expiration date: {self._expiration_date}, cooking time: {self._cooking_time}, total kcal: {self._total_kcal}"
