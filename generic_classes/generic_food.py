1f733e1aba2c310ace0fe7d22062c136dfb7f7a6
Sara Gironi <sara.gironi97@gmail.com>
2025-03-14 12:27:20 +0100

    compliance with mypy

--- generic_classes/generic_food.py
+++ generic_classes/generic_food.py
@@ -7,22 +7,6 @@ class GenericFood:
     name: str
     expiration_date: datetime
 
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @name.setter
-    def name(self, name: str) -> None:
-        self._name = name
-
-    @property
-    def expiration_date(self) -> datetime:
-        return self._expiration_date
-
-    @expiration_date.setter
-    def expiration_date(self, expiration_date: datetime) -> None:
-        self._expiration_date = expiration_date
-
     def __str__(self) -> str:
         return f"Type of food: {self._name}, expiration date: {self._expiration_date}"
 
--- generic_classes/generic_recipe.py
+++ generic_classes/generic_recipe.py
@@ -1,55 +1,15 @@
 from dataclasses import dataclass
 from datetime import datetime
 from generic_classes.nutritional_info import NutritionalInformation
-
+from generic_classes.generic_food import GenericFood
 
 @dataclass
 class GenericRecipe:
     name: str
-    ingredients: list
+    ingredients: list[GenericFood]
     expiration_date: datetime
     cooking_time: int
     nutritional_info: NutritionalInformation
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @name.setter
-    def name(self, name: str) -> None:
-        self._name = name
-
-    @property
-    def ingredients(self) -> list:
-        return self._ingredients
-
-    @ingredients.setter
-    def ingredients(self, ingredients: list) -> None:
-        self._ingredients = ingredients
-
-    @property
-    def expiration_date(self) -> datetime:
-        return self._expiration_date
-
-    @expiration_date.setter
-    def expiration_date(self, expiration_date: datetime) -> None:
-        self._expiration_date = expiration_date
-
-    @property
-    def cooking_time(self) -> int:
-        return self._cooking_time
-
-    @cooking_time.setter
-    def cooking_time(self, cooking_time: int) -> None:
-        self._cooking_time = cooking_time
-
-    @property
-    def nutritional_info(self) -> NutritionalInformation:
-        return self._nutritional_info
-
-    @nutritional_info.setter
-    def nutritional_info(self, nutritional_info: NutritionalInformation) -> None:
-        self._nutritional_info = nutritional_info
 
     def __str__(self) -> str:
         return f"Recipe name: {self._name}, ingredients: {self._ingredients}, expiration date: {self._expiration_date}, cooking time: {self._cooking_time}, nutritional information: {self._nutritional_info}."
